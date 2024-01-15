66fd306c0c8b6c280cd7dd871ab8b4090509a209
Nikita Mikhailovskiy <nmihaylovskiy@gmail.com>
2024-01-15 18:00:49 +0600

    Update description

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "project.h"
+
+int
+main(int argc, char const *argv[]) {
+    struct ProjectInfo info = {};
+
+    std::cout << info.nameString << "\t" << info.versionString << '\n';
+
+    (void) argc;
+    (void) argv;
+    return 0;
+}
